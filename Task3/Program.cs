4cc661f32937062079a9372dbb62c28cdd2b48f3
Аркадий Кунаев <kunayev@mail.ru>
2022-01-16 17:28:06 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Дана строка S. Из строки требуется удалить текст, заключенный в фигурные скобки.
+// В строке может быть несколько фрагментов, заключённых в фигурные скобки.
+// Возможно использование вложенных фигурных скобок, необходимо, чтобы программа это учитывала.
+
+string S = Console.ReadLine();
+
+int level = 0;
+
+string S2 = "";
+
+foreach (char c in S)
+{
+    switch (c)
+    {
+        case '{':
+            level++;
+            break;
+        case '}':
+            level--;
+            if(level<0)
+            {
+                Console.WriteLine("Ошибка во входной строке!");
+                return;
+            }
+            break;
+        default:
+            if (level == 0)
+                S2 += c;
+            break;
+    }
+}
+
+Console.WriteLine(S2);
+Console.ReadKey();
