0443624e0b669003b150f2c0fc2cb326da4b1aef
Аркадий Кунаев <kunayev@mail.ru>
2022-01-16 17:11:08 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Ввести с клвиатуры предложение.
+//Предложение представляет собой слова, разделенные пробелом.
+//Знаки препинания не используются.
+//Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»)
+
+string str = Console.ReadLine();
+string strWithoutSpaces = "";
+
+string[] strArray = str.Split();
+
+foreach (string str2 in strArray)
+{
+    strWithoutSpaces += str2;
+}
+
+strWithoutSpaces = strWithoutSpaces.ToLower();
+
+bool flag = true;
+for (int i = 0; i < strWithoutSpaces.Length/2; i++)
+{
+    if(strWithoutSpaces[i] != strWithoutSpaces[strWithoutSpaces.Length-1-i])
+    { flag= false; break; }
+}
+
+if (flag)
+{
+    Console.WriteLine("Фраза является палиндромом");
+}
+else
+{
+    Console.WriteLine("Фраза не является палиндромом");
+}
+
+Console.ReadKey();
