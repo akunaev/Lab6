e9b001073366a8e1b1964459f956c0c23d985891
Аркадий Кунаев <kunayev@mail.ru>
2022-01-16 16:59:49 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Ввести с клавиатуры предложение.
+//Предложение представляет собой слова, разделенные пробелом.
+//Знаки препинания не используются. Найти самое длинное слово в строке.
+
+string str = Console.ReadLine();
+string[] strArray = str.Split();
+
+int max = 0;
+string maxStr = "";
+foreach (string str2 in strArray)
+{
+    if (str2.Length > max)
+    {
+        max = str2.Length;
+        maxStr = str2;
+    }
+}
+
+Console.WriteLine(maxStr);
